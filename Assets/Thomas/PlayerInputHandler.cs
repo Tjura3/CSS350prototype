be6e8773ff7361606a1e3f55443257a460cb4f10
Thomas Juranich <tjuranich777@gmail.com>
2026-03-10 00:21:41 -0700

    Movement stuff is done and small main menu edit

--- Assets/Thomas/PlayerInputHandler.cs
+++ Assets/Thomas/PlayerInputHandler.cs
@@ -6,8 +6,10 @@ public class PlayerInputHandler : MonoBehaviour
     [Header("Input Action Asset")]
     [SerializeField] private InputActionAsset playerControls;
 
-    [Header("Input Action Asset")]
+
+    [Header("Action Map Name Reference")]
     [SerializeField] private string actionMapName = "Player";
+
 
     [Header("Action Name References")]
     [SerializeField] private string movement = "Movement";
@@ -15,53 +17,61 @@ public class PlayerInputHandler : MonoBehaviour
     [SerializeField] private string jump = "Jump";
     [SerializeField] private string sprint = "Sprint";
 
+
     private InputAction movementAction;
     private InputAction rotationAction;
     private InputAction jumpAction;
     private InputAction sprintAction;
+
 
     public Vector2 MovementInput { get; private set; }
     public Vector2 RotationInput { get; private set; }
     public bool JumpTriggered { get; private set; }
     public bool SprintTriggered { get; private set; }
 
+
     private void Awake()
     {
         InputActionMap mapReference = playerControls.FindActionMap(actionMapName);
+
 
         movementAction = mapReference.FindAction(movement);
         rotationAction = mapReference.FindAction(rotation);
         jumpAction = mapReference.FindAction(jump);
         sprintAction = mapReference.FindAction(sprint);
 
+
         SubscribeActionValuesToInputEvents();
     }
+
 
     private void SubscribeActionValuesToInputEvents()
     {
         movementAction.performed += inputInfo => MovementInput = inputInfo.ReadValue<Vector2>();
         movementAction.canceled += inputInfo => MovementInput = Vector2.zero;
 
+
         rotationAction.performed += inputInfo => RotationInput = inputInfo.ReadValue<Vector2>();
-        movementAction.canceled += inputInfo => RotationInput = Vector2.zero;
+        rotationAction.canceled += inputInfo => RotationInput = Vector2.zero;
+
 
         jumpAction.performed += inputInfo => JumpTriggered = true;
         jumpAction.canceled += inputInfo => JumpTriggered = false;
 
+
         sprintAction.performed += inputInfo => SprintTriggered = true;
         sprintAction.canceled += inputInfo => SprintTriggered = false;
-
-
-
     }
+
 
     private void OnEnable()
     {
         playerControls.FindActionMap(actionMapName).Enable();
     }
+
+
     private void OnDisable()
     {
         playerControls.FindActionMap(actionMapName).Disable();
     }
-
 }
